6194ca4369f98c651b65311cc8baee8e3d748d9b
raspi <dateivis@yahoo.com>
2019-03-13 22:32:46 +0000

    jesus christ that was difficult to set up. test 1 did not work, gpio library errors

--- config/index.js
+++ config/index.js
@@ -5,15 +5,19 @@
 const path = require('path')
 
 module.exports = {
+devServer: {
+    host: "raspberrypi", // Your Computer Name
+    port: 8080
+},
   dev: {
 
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
-
+	inline:true,
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
@@ -73,4 +77,5 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   }
+
 }
