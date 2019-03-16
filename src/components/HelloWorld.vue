65d37720e0c76207e3418cc7d077f01615ee0d33
Gytis <geceviciusgytis@gmail.com>
2019-03-16 19:33:40 +0000

    another quick fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -82,8 +82,8 @@ methods : {
       eval(code);
     } catch (e) {
       alert(e);
-    }
-  },*/
+    }*/
+  },
   setOutput : function(){
     var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
