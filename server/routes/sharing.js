187ac583968e95cc88f89905d1725d62d602a823
Gytis <geceviciusgytis@gmail.com>
2019-04-27 15:07:41 +0100

    requiring body parser directly

--- server/routes/sharing.js
+++ server/routes/sharing.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var ip = require('ip');
 const storage = require('node-persist');
 storage.init();
+const bodyParser = require('body-parser');
 
-
-router.post('/', express.bodyParser({limit: '15mb'}),async function(req, res, next) {
+router.post('/', bodyParser({limit: '15mb'}),async function(req, res, next) {
 	if(req.connection.remoteAddress === "127.0.0.1"){
 		await storage.setItem('enabled',req.body.enabled);
 		await storage.setItem('workspace',req.body.workspace);
