7fd5a0f69ef328809d7764bfc7a9c8cf07dffa00
Gytis <geceviciusgytis@gmail.com>
2019-04-27 15:37:20 +0100

    io fix

--- server/routes/sharing.js
+++ server/routes/sharing.js
@@ -4,8 +4,10 @@ var ip = require('ip');
 const storage = require('node-persist');
 storage.init();
 const bodyParser = require('body-parser');
+const io = req.app.get('socketio');
 
 router.post('/',async function(req, res, next) {
+
 	var enabled;
 		if(req.body.enabled){
 			await storage.setItem('enabled',req.body.enabled);
@@ -13,11 +15,11 @@ router.post('/',async function(req, res, next) {
 			io.emit("wsSharing",{enabled:req.body.enabled,url:url,msg:"Sharing enabled!"});
 		}
 		else{
-			
+
 			io.emit("wsUpdated",{msg:"Workspace updated."});
 		}
 		await storage.setItem('workspace',req.body.workspace);
-		const io = req.app.get('socketio');
+		
 		var url = ip.address();
 		
 		res.sendStatus(200)
